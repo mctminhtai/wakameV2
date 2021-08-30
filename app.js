1a0c4682e11c78d8e10855469a9c08f5e67911df
DESKTOP-BEJM7Q0\tailm <mct.minhtai@gmail.com>
2021-08-30 22:07:50 +0700

    break layout

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var expressLayouts = require('express-ejs-layouts');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -21,9 +22,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(expressLayouts);
 app.use(logger('dev'));
 app.use(express.json({ limit: '150mb' }));
-app.use(express.urlencoded({ extended: true,limit:'150mb' }));
+app.use(express.urlencoded({ extended: true, limit: '150mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
@@ -41,7 +43,6 @@ app.use('/', homeRouter);
 app.use('/blog', blogsRouter);
 app.use('/accounts', accountsRouter);
 app.use('/upload', uploadFileRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -71,6 +72,5 @@ connection();
 // 	// we're connected!
 // 	console.log('we are connected!');
 // });
-
 
 module.exports = app;
