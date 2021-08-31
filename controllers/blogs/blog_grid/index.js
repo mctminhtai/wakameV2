2af62c56f2f8627e2b50e977beb97cf3eb968ae2
DESKTOP-BEJM7Q0\tailm <mct.minhtai@gmail.com>
2021-09-01 02:37:33 +0700

    phan trang cho blog grid

--- controllers/blogs/blog_grid/index.js
+++ controllers/blogs/blog_grid/index.js
@@ -1,11 +1,31 @@
-var { Blogs,Tags,Categories } = require('../../../models/index');
+var { Blogs, Tags, Categories } = require('../../../models/index');
 const blogGridIndex = async function (req, res, next) {
-	var [blogs,tags,categories] = await Promise.all([
+	var [blogs, tags, categories] = await Promise.all([
 		Blogs.find({}, 'title description category').populate({ path: 'category', select: 'name' }),
 		Tags.find({}),
 		Categories.find({}),
 	]);
-	return res.render('blog-grid', { blogs,tags,categories, user: req.user });
+	var itemPerPage = 8;
+	var pageCount = Math.ceil(blogs.length / itemPerPage);
+	if (req.query.page) {
+		if(req.query.page > pageCount){
+			return res.redirect('/blog?page=1');
+		}
+		if(req.query.page < 1){
+			return res.redirect('/blog?page=' + pageCount);
+		}
+	}
+	var currentPage = req.query.page || 1;
+	var begin = (currentPage - 1) * itemPerPage;
+	var end = currentPage * itemPerPage;
+	console.log(begin, end, pageCount);
+	return res.render('blog-grid', {
+		blogs: blogs.slice(begin, end),
+		tags,
+		categories,
+		user: req.user,
+		pagination: { pageCount, currentPage },
+	});
 };
 module.exports = {
 	blogGridIndex,
--- controllers/blogs/blog_single/index.js
+++ controllers/blogs/blog_single/index.js
@@ -1,9 +1,14 @@
-var { Blogs } = require('../../../models/blogs');
+var { Blogs,Categories,Tags } = require('../../../models/index');
 exports.getBlogSingle = async function (req, res, next) {
 	var blogId = req.params.blogid;
-	var blog = await Blogs.findById(blogId)
-		.populate({ path: 'category', select: 'name' })
-		.populate({ path: 'userId', select: 'fullName' });
-	console.log(blog);
-	return res.render('blog-detail', { blog, user: req.user });
+	// var blog = await Blogs.findById(blogId)
+	// 	.populate({ path: 'category', select: 'name' })
+	// 	.populate({ path: 'userId', select: 'fullName' });
+	var [blogs, tags, categories,blog] = await Promise.all([
+		Blogs.find({}, 'title description category').populate({ path: 'category', select: 'name' }),
+		Tags.find({}),
+		Categories.find({}),
+		Blogs.findById(blogId).populate({ path: 'category', select: 'name' }).populate({ path: 'userId', select: 'fullName' }),
+	]);
+	return res.render('blog-detail', { blog, blogs, tags, categories, user: req.user });
 };
