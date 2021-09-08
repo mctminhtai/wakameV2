d3db7c351ce933a5c01dcea3d1c821b1a6c3c39e
DESKTOP-BEJM7Q0\tailm <mct.minhtai@gmail.com>
2021-09-08 15:05:41 +0700

    hoan thanh sua noi dung trang chu

--- controllers/home/index.js
+++ controllers/home/index.js
@@ -1,3 +1,8 @@
-exports.index = function (req, res, next) {
-	res.render('index', { user: req.user });
+var { Blogs } = require('../../models/index');
+exports.index = async function (req, res, next) {
+	let lastedBlogs = await Blogs.find({ status: true }, 'title description category dateCreated readCount thumbImg')
+		.populate({ path: 'category', select: 'name' })
+		.sort({ dateCreated: 'desc' });
+	lastedBlogs = lastedBlogs.slice(0, 3);
+	res.render('index', { user: req.user, blogs: lastedBlogs });
 };
