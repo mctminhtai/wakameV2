f8889a34c5680d87bcc017e4697a8ce8b83199e0
mctminhtai <mct.minhtai@gmail.com>
2021-09-14 17:42:35 +0100

    tao folder khi khong ton tai

--- controllers/upload/multerDefine.js
+++ controllers/upload/multerDefine.js
@@ -1,6 +1,13 @@
 const multer = require('multer');
+const fs = require("fs");
 var storage = multer.diskStorage({
 	destination: function(req,file,cb){
+		if(fs.existsSync('./filesUploaded')){
+			// console.log('Da ton tai folder roi');
+		}else{
+			fs.mkdirSync('./filesUploaded');
+			// console.log('da tao thanh cong folder');
+		}
 		cb(null, './filesUploaded');
 	},
 	filename: function (req, file, cb) {
